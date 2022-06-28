2c7e4fe606378d0390e32d2f02862dac8ff22470
Marty De Alba <103968668+martydealba27@users.noreply.github.com>
2022-06-28 06:21:45 -0700

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,8 @@
+#print list and say hi to each friend in list 
+friends = ["Taylor", "Alex", "Marty", "Haylee","testing"]
+for friend in friends:
+    print("Hi " + friend)
+
+#print we are testing automation 10 times 
+for i in range(10):
+    print("We are testing automation")
