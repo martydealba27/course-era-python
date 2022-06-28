3cc71135e11e1b317ec0ca84ef87a2807f0ed406
Marty De Alba <103968668+martydealba27@users.noreply.github.com>
2022-06-28 16:14:18 -0700

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,36 @@
+#print
+#print is a python function
+
+#function
+print("Python syntax is important")
+
+name = "Marty"
+print("Hello " + name)
+
+color = "red"
+
+thing = "clown nose"
+
+print(color + " is the color of " + thing)
+
+print(45%10)
+
+print(((2050/5)-32)/9)
+
+print(2**3) 
+
+
+ratio = (1+(5**(1/2)))/2
+print(ratio)
+
+
+seconds_day = 86400
+days_per_week = 7
+seconds_per_week = seconds_day * days_per_week
+print(seconds_per_week)
+
+disk_size = 16*1024*1024*1024
+sector_size = 512
+sector_amount = disk_size/sector_size
+print(sector_amount)
+      
